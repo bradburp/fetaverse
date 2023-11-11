2820f4b52dc1fd762734df40e349ef5257cc0680
bradburp <paul.bradbury@live.co.uk>
2023-11-11 12:01:39 +0000

    Initial commit

---
+++ src/main/java/com/world/bradburp/fetaverse/FetaverseApplication.java
@@ -0,0 +1,13 @@
+package com.world.bradburp.fetaverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetaverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetaverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/world/bradburp/fetaverse/FetaverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.world.bradburp.fetaverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetaverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
